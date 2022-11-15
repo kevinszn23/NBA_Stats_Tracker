b108314438f167769f235918335fd071d7ebd6e2
Kevin Ng <kehfunn@gmail.com>
2022-11-14 19:06:28 -0500

    searching for specific player and player id is working

--- nba.py
+++ nba.py
@@ -1,4 +1,9 @@
 from nba_api.stats.static import players
 
 player_dict = players.get_players()
-print(f"{player_dict}")
+# print(f"{player_dict}")
+
+lebron = [player for player in player_dict if player["full_name"] == "LeBron James"][0]
+print(f"{lebron}")
+lebron_id = lebron["id"]
+print(f"{lebron_id}")
