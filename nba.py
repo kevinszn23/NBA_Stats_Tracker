c65fe08851e321b1ec580ffa213d652009b09e27
Kevin Ng <kehfunn@gmail.com>
2022-11-14 19:45:17 -0500

    finding teams is working, team id also working

--- nba.py
+++ nba.py
@@ -5,5 +5,17 @@ player_dict = players.get_players()
 
 lebron = [player for player in player_dict if player["full_name"] == "LeBron James"][0]
 print(f"{lebron}")
+
 lebron_id = lebron["id"]
 print(f"{lebron_id}")
+
+from nba_api.stats.static import teams
+
+team_dict = teams.get_teams()
+# print(f"{team_dict}")
+
+gsw = [team for team in team_dict if team["full_name"] == "Golden State Warriors"][0]
+print(f"{gsw}")
+
+gsw_id = gsw["id"]
+print(f"{gsw_id}")
