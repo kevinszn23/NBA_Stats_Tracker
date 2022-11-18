e77d415e75ee7e0e55cd3bce0dcf9fe51edd5b72
Kevin Ng <kehfunn@gmail.com>
2022-11-18 12:59:29 -0500

    messing around with api

--- nba.py
+++ nba.py
@@ -5,25 +5,33 @@ from nba_api.stats.static import players
 player_dict = players.get_players()
 # print(f"{player_dict}")
 
-lebron = [player for player in player_dict if player["full_name"] == "LeBron James"][0]
-print(f"{lebron}")
+# lebron = [player for player in player_dict if player["full_name"] == "LeBron James"][0]
+# print(f"{lebron}")
 
-lebron_id = lebron["id"]
-print(f"{lebron_id}")
+# lebron_id = lebron["id"]
+# print(f"{lebron_id}")
 
-from nba_api.stats.static import teams
+# from nba_api.stats.static import teams
 
-team_dict = teams.get_teams()
-# print(f"{team_dict}")
+# team_dict = teams.get_teams()
+# # print(f"{team_dict}")
 
-gsw = [team for team in team_dict if team["full_name"] == "Golden State Warriors"][0]
-print(f"{gsw}")
+# gsw = [team for team in team_dict if team["full_name"] == "Golden State Warriors"][0]
+# print(f"{gsw}")
 
-gsw_id = gsw["id"]
-print(f"{gsw_id}")
+# gsw_id = gsw["id"]
+# print(f"{gsw_id}")
 
-from nba_api.stats.endpoints import playergamelog
+# from nba_api.stats.endpoints import playergamelog
 
-gamelog_lebron = playergamelog.PlayerGameLog(player_id="2544")
-gamelog_lebron_df = gamelog_lebron.get_data_frames()[0]
-print(f"{gamelog_lebron_df}")
+# gamelog_lebron = playergamelog.PlayerGameLog(player_id="2544")
+# gamelog_lebron_df = gamelog_lebron.get_data_frames()[0]
+# print(f"{gamelog_lebron_df}")
+
+from nba_api.stats.endpoints import playercareerstats
+
+ppg = playercareerstats.PlayerCareerStats(player_id="2544")
+ppg.get_data_frames()[0]
+ppg.get_json()
+ppg.get_dict()
+print(f"{ppg.get_dict()}")
