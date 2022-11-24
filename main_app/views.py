bf1c9afe766fd972e8c54511df595a9dbd0635fc
Kevin Ng <kehfunn@gmail.com>
2022-11-24 13:29:04 -0500

    trying to display api data on page

--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-from nba_api.stats.endpoints import playercareerstats
+# from nba_api.stats.endpoints import playercareerstats
+# from django.http import JsonResponse
 
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
 
-
+# def playerstats(request):
+#     dfs = playercareerstats.PlayerCareerStats(
+#             player_id="2544", per_mode36="PerGame", league_id_nullable="00"
+# ).get_data_frames()[0]
+#     return JsonResponse(dfs, safe=False)
