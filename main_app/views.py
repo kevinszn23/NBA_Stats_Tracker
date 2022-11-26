caedfaf35bbb3e7599587693082470140edc2afb
Kevin Ng <kehfunn@gmail.com>
2022-11-26 15:28:32 -0500

    testing displaying api data

--- main_app/views.py
+++ main_app/views.py
@@ -4,13 +4,23 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 # from nba_api.stats.endpoints import playercareerstats
 # from django.http import JsonResponse
+import requests
 
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
 
-# def playerstats(request):
-#     dfs = playercareerstats.PlayerCareerStats(
-#             player_id="2544", per_mode36="PerGame", league_id_nullable="00"
-# ).get_data_frames()[0]
-#     return JsonResponse(dfs, safe=False)
+def show_page(request):
+    all_players_url = "https://stats.nba.com/stats/playercareerstats"
+    response_all_players = requests.get(all_players_url)
+    temp_all_players = response_all_players.json()
+    result_all_players = temp_all_players['data']
+    
+    for i in result_all_players:
+        player_data = Player(
+            player_id = i['id'],
+            first_name = i['first_name'],
+            last_name = i['last_name']
+        )
+        player_data.save()
+        all_players = Player.objects.all().order_by('-id')
