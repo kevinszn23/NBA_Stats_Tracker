80fd59e501387f285dd5f1f100229901d1fde8b9
Kevin Ng <kehfunn@gmail.com>
2022-11-26 15:43:46 -0500

    added Player model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    player_id = models.IntegerField()
+    first_name = models.CharField(max_length = 50, blank = True, null = True)
+    last_name = models.CharField(max_length = 50, blank = True, null = True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
