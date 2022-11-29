783f18321f103beedea8ef651105fcbff8c51ba4
Kevin Ng <kehfunn@gmail.com>
2022-11-29 12:36:31 -0500

    trying to display api data on home page

--- main_app/views.py
+++ main_app/views.py
@@ -5,17 +5,19 @@ from django.views.generic.base import TemplateView
 # from nba_api.stats.endpoints import playercareerstats
 # from django.http import JsonResponse
 import requests
+from main_app.models import Player
 
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
 
 def show_page(request):
+    all_players = {}
     all_players_url = "https://stats.nba.com/stats/playercareerstats"
     response_all_players = requests.get(all_players_url)
     temp_all_players = response_all_players.json()
     result_all_players = temp_all_players['data']
-    
+
     for i in result_all_players:
         player_data = Player(
             player_id = i['id'],
@@ -23,4 +25,5 @@ def show_page(request):
             last_name = i['last_name']
         )
         player_data.save()
-        all_players = Player.objects.all().order_by('-id')
+    all_players = Player.objects.all().order_by('-id')
+    return render(request, "show_page.html", {"all_players": result_all_players})
