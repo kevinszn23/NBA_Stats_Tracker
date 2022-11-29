f1c4d0394a0900b24b41ded3183c50647bd0f783
Kevin Ng <kehfunn@gmail.com>
2022-11-29 18:10:25 -0500

    testing different api to try to display

--- main_app/views.py
+++ main_app/views.py
@@ -2,28 +2,54 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-# from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import playercareerstats
 # from django.http import JsonResponse
 import requests
 from main_app.models import Player
+# import sys
 
 # Create your views here.
-# class Home(TemplateView):
-#     template_name = "home.html"
+class Home(TemplateView):
+    template_name = "home.html"
+    model = Player
 
-def home(request):
-    all_players = {}
-    all_players_url = "https://www.nba.com/stats/player/201142"
-    response_all_players = requests.get(all_players_url)
-    temp_all_players = response_all_players.json()
-    result_all_players = temp_all_players['data']
+    # def nba_api_test(request):
+    #     dfs = playercareerstats.PlayerCareerStats(
+    #             player_id="2544", per_mode36="PerGame", league_id_nullable="00"
+    # ).get_data_frames()[0]
+    #     print(f"{dfs}")
 
-    for i in result_all_players:
-        player_data = Player(
-            player_id = i['id'],
-            first_name = i['first_name'],
-            last_name = i['last_name']
-        )
-        player_data.save()
-    all_players = Player.objects.all().order_by('-id')
-    return render(request, "home.html", {"all_players": all_players})
+    #     lcs = dfs.iloc[19]
+    #     print(f"{lcs}")
+    #     return render(request, "home.html")
+    
+    def balldontdie(request):
+        requests.get("https://www.balldontlie.io/api/v1/players/237")
+        data = response.json()
+        return render(request, "home.html", {
+            "nba": data["display"]
+        })
+
+
+# def home(request):
+#     all_players = {}
+#     all_players_url = "https://stats.nba.com/stats/playercareerstats?LeagueID=&PerMode=Totals&PlayerID=2544"
+#     response_all_players = requests.get(all_players_url)
+#     temp_all_players = response_all_players.json()
+#     result_all_players = temp_all_players['data']
+
+#     for i in result_all_players:
+#         player_data = Player(
+#             player_id = i['id'],
+#             first_name = i['first_name'],
+#             last_name = i['last_name']
+#         )
+#         player_data.save()
+#     all_players = Player.objects.all().order_by('-id')
+#     return render(request, "home.html", {"all_players": all_players})
+
+# nba = "https://stats.nba.com/stats/playercareerstats?LeagueID=&PerMode=Totals&PlayerID=2544"
+
+# # "https://stats.nba.com/stats/playercareerstats"
+
+# print(requests.get(nba).content)
